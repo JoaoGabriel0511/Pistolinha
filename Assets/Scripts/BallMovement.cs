9fac7af8139d306bbb897ea1156166740d0ad74e
mashiro11 <thiagoaas1990@gmail.com>
2019-08-03 02:50:21 -0300

    Ball

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    //  Internal references
+    Rigidbody2D rb2D;
+
+    //  attributes
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        rb2D.velocity = speed * direction.normalized;
+    }
+}
