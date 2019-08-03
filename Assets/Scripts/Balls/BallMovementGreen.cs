562c689e66388858d7e14434d636378bd36b5b14
bruno takashi <bt.tengan@gmail.com>
2019-08-03 16:09:48 -0300

    Adjust ball collision and death

--- Assets/Scripts/Balls/BallMovementGreen.cs
+++ Assets/Scripts/Balls/BallMovementGreen.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
 
 public class BallMovementGreen : BallMovement {
-    /*
 	public override void OnTriggerEnter2D(Collider2D other) {
 		if (other.GetComponent<IColorful>() != null) {
 			Wall wall = other.GetComponent<Wall>();
 			if (wall != null) {
 				switch (wall.GetColor()) {
 					case Constants.Type.GREEN:
-						Destroy(gameObject);
+						StartCoroutine("MakeDeath", wall);
 						break;
 					case Constants.Type.RED:
-						ColisionWithWall(wall.Angle);
+						StartCoroutine("MakeColision", wall);
 						break;
 					case Constants.Type.BLUE:
 						break;
 				}
 			}
 		}
-	}*/
+	}
 }
