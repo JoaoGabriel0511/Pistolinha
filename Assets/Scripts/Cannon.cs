0dfb7c3b1d1d15c0748d68bd6bd6df42d5da6844
bruno takashi <bt.tengan@gmail.com>
2019-08-04 05:04:39 -0300

    Cannon enabled colors option

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -9,6 +9,10 @@ public class Cannon : MonoBehaviour {
 	[SerializeField] GameObject ballPrefab;
 	[SerializeField] BallCollisionBehaviour[] ballBehaviours;
 
+	[SerializeField] bool _redEnable = true;
+	[SerializeField] bool _blueEnable = true;
+	[SerializeField] bool _greenEnable = true;
+
 	Constants.Type selectedColor = Constants.Type.RED;
 	SpriteRenderer _spriteRenderer;
 
@@ -16,12 +20,32 @@ public class Cannon : MonoBehaviour {
 	public string ShotEvent;
 	FMOD.Studio.EventInstance Shot;
 
+	[SerializeField] int _typeVar = (int)Constants.Type.RED;
+
 	void Awake() {
 		_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 		if (!_spriteRenderer) {
 			Debug.Log("No SpriteRenderer found.");
 		}
-		_spriteRenderer.color = Color.red;
+
+		if (_redEnable) {
+			_spriteRenderer.color = Color.red;
+			selectedColor = Constants.Type.RED;
+			_typeVar = (int)Constants.Type.RED;
+		}
+		else if (_blueEnable) {
+			_spriteRenderer.color = Color.blue;
+			selectedColor = Constants.Type.BLUE;
+			_typeVar = (int)Constants.Type.BLUE;
+		}
+		else if (_greenEnable) {
+			_spriteRenderer.color = Color.green;
+			selectedColor = Constants.Type.GREEN;
+			_typeVar = (int)Constants.Type.GREEN;
+		}
+		else {
+			Debug.Log("No color enabled in the cannon!");
+		}
 	}
 
 	void Update() {
@@ -37,6 +61,7 @@ public class Cannon : MonoBehaviour {
 	}
 
 	private void SwitchToPreviousBall() {
+
 		switch (selectedColor) {
 			case Constants.Type.RED:
 				selectedColor = Constants.Type.GREEN;
