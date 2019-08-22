fde3ae9a4367c7a9ce5fdd9bcb02d53c69b69b65
JoaoGabrie0511 <joaogabriel2010@hotmail.com>
2019-08-22 01:28:25 -0300

    painel de fase selecionada

---
+++ Assets/stagePanel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stagePanel : MonoBehaviour
+{
+    public int level;
+}
---
+++ Assets/startLevelButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startLevelButton : MonoBehaviour
+{
+    public void onClick() {
+        int level = GetComponentInParent<stagePanel>().level;
+        if (level <= GameManager.Instance.LastPlayed())
+        {
+            GameManager.Instance.LoadScene("Level"+level);
+        }
+    }
+}
