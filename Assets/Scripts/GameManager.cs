cc56a60ebdc9a3f949c1858dd94397c886fa32e3
bruno takashi <bt.tengan@gmail.com>
2019-08-24 23:52:28 -0300

    Configurado resolução inicial do jogo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,143 +4,121 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
-    public static GameManager Instance;
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance;
 
-    void Awake()
-    {
-        if(Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Debug.LogWarning("Two instances of GameManager! Destroying gameObject");
-            Destroy(gameObject);
-        }
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        /*
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+		}
+		else {
+			Debug.LogWarning("Two instances of GameManager! Destroying gameObject");
+			Destroy(gameObject);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+		/*
         BGMusic = FMODUnity.RuntimeManager.CreateInstance(BGEvent);
         BGMusic.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         BGMusic.start();
         */
-        SceneManager.LoadScene("TitleScreen", LoadSceneMode.Additive);
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
 
-    internal void LoadSameLevel() {
-        int currentLevel = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
-        LoadScene("Level" + (currentLevel));
-    }
+		Screen.SetResolution(360, 640, false);
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.name != "GameGUI") SceneManager.SetActiveScene(scene);
+		SceneManager.LoadScene("TitleScreen", LoadSceneMode.Additive);
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
 
-        //if (scene.name.Contains("Level")) SceneManager.LoadScene("GameGUI");
-    }
+	internal void LoadSameLevel() {
+		int currentLevel = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
+		LoadScene("Level" + (currentLevel));
+	}
 
-    public void StartGame()
-    {
-        // LoadScene("LoadingScreen");
-        if (LastPlayed() == 0)
-        {
-            StageCleared(0);
-            LoadScene("Level1");
-        }
-        else
-        {
-            LoadScene("WorldSelect");
-        }
-    }
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (scene.name != "GameGUI") SceneManager.SetActiveScene(scene);
 
-    public void UnloadActiveScene()
-    {
-        Scene scene = SceneManager.GetActiveScene();
-        if (scene.buildIndex == 0) return;
+		//if (scene.name.Contains("Level")) SceneManager.LoadScene("GameGUI");
+	}
 
-        if (scene.name.Contains("Level"))
-        {
-            SceneManager.UnloadSceneAsync("GameGUI");
-        }
-        SceneManager.UnloadSceneAsync(scene.buildIndex);
-    }
+	public void StartGame() {
+		// LoadScene("LoadingScreen");
+		if (LastPlayed() == 0) {
+			StageCleared(0);
+			LoadScene("Level1");
+		}
+		else {
+			LoadScene("WorldSelect");
+		}
+	}
 
-    public void LoadScene(string scene)
-    {
-        //SceneManager.LoadScene(scene, LoadSceneMode.Single);
-        UnloadActiveScene();
-        if (scene.Contains("Level"))
-        {
-            int level;
-            int.TryParse(scene.Remove(0, "Level".Length), out level);
-            if (level > PlayerPrefs.GetInt("LastPlayed"))
-            {
-                PlayerPrefs.SetInt("LastPlayed", level);
-            }
-            //StartCoroutine(LoadSceneAsync(scene));
-            SceneManager.LoadScene("GameGUI", LoadSceneMode.Additive);
-            SceneManager.LoadScene(scene, LoadSceneMode.Additive);
-        }
-        else
-        {
-            SceneManager.LoadScene(scene, LoadSceneMode.Additive);
-        }
-    }
+	public void UnloadActiveScene() {
+		Scene scene = SceneManager.GetActiveScene();
+		if (scene.buildIndex == 0) return;
 
-    public IEnumerator LoadSceneAsync(string scene)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
-        while (!operation.isDone)
-        {
-            yield return null;
-        }
-        SceneManager.LoadScene("GameGUI", LoadSceneMode.Additive);
-    }
+		if (scene.name.Contains("Level")) {
+			SceneManager.UnloadSceneAsync("GameGUI");
+		}
+		SceneManager.UnloadSceneAsync(scene.buildIndex);
+	}
 
-    public int LastPlayed()
-    {
-        return PlayerPrefs.GetInt("LastPlayed", 0);
-    }
+	public void LoadScene(string scene) {
+		//SceneManager.LoadScene(scene, LoadSceneMode.Single);
+		UnloadActiveScene();
+		if (scene.Contains("Level")) {
+			int level;
+			int.TryParse(scene.Remove(0, "Level".Length), out level);
+			if (level > PlayerPrefs.GetInt("LastPlayed")) {
+				PlayerPrefs.SetInt("LastPlayed", level);
+			}
+			//StartCoroutine(LoadSceneAsync(scene));
+			SceneManager.LoadScene("GameGUI", LoadSceneMode.Additive);
+			SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+		}
+		else {
+			SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+		}
+	}
 
-    public void ClearProgress()
-    {
-        PlayerPrefs.SetInt("LastPlayed", 0);
-    }
+	public IEnumerator LoadSceneAsync(string scene) {
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+		while (!operation.isDone) {
+			yield return null;
+		}
+		SceneManager.LoadScene("GameGUI", LoadSceneMode.Additive);
+	}
 
-    public void ResetScene()
-    {
-        LoadScene(SceneManager.GetActiveScene().name);
-    }
+	public int LastPlayed() {
+		return PlayerPrefs.GetInt("LastPlayed", 0);
+	}
 
-    public void StageCleared()
-    {
-        int stage = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
-        if(stage == PlayerPrefs.GetInt("LastPlayed"))
-        {
-            PlayerPrefs.SetInt("LastPlayed", stage + 1);
-        }
-    }
+	public void ClearProgress() {
+		PlayerPrefs.SetInt("LastPlayed", 0);
+	}
 
-    public void StageCleared(int zero)
-    {
-        PlayerPrefs.SetInt("LastPlayed", 1);
-    }
+	public void ResetScene() {
+		LoadScene(SceneManager.GetActiveScene().name);
+	}
 
-    public void LoadNextLevel()
-    {
-        int currentLevel = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
-        if(currentLevel < 10)
-            LoadScene("Level"+(currentLevel+1));
-        else
-        {
-            LoadScene("WorldSelect");
-        }
-    }
+	public void StageCleared() {
+		int stage = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
+		if (stage == PlayerPrefs.GetInt("LastPlayed")) {
+			PlayerPrefs.SetInt("LastPlayed", stage + 1);
+		}
+	}
+
+	public void StageCleared(int zero) {
+		PlayerPrefs.SetInt("LastPlayed", 1);
+	}
+
+	public void LoadNextLevel() {
+		int currentLevel = int.Parse(SceneManager.GetActiveScene().name.Remove(0, "Level".Length));
+		if (currentLevel < 10)
+			LoadScene("Level" + (currentLevel + 1));
+		else {
+			LoadScene("WorldSelect");
+		}
+	}
 
 }
