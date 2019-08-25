bb2ca3200445d3b364b8da987153bb70310b1db7
JoaoGabrie0511 <joaogabriel2010@hotmail.com>
2019-08-25 19:26:37 -0300

    tela de creditos feita

---
+++ Assets/GoToCredits.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToCredits : MonoBehaviour
+{
+    public void OnClick() {
+        GameManager.Instance.LoadScene("Credits");
+    }
+}
